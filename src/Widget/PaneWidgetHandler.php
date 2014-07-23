8da692ec813a55d8dc725c67bbb11993f2555e0c
crynobone <crynobone@gmail.com>
2014-07-24 07:04:33 +0800

    Implement use Orchestra\Support\Str.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Widget/PaneWidgetHandler.php
+++ src/Widget/PaneWidgetHandler.php
@@ -1,5 +1,7 @@
 <?php namespace Orchestra\Widget;
 
+use Orchestra\Support\Str;
+
 class PaneWidgetHandler extends Factory
 {
     /**
@@ -24,7 +26,7 @@ class PaneWidgetHandler extends Factory
      */
     public function add($id, $location = '#', $callback = null)
     {
-        if (is_string($location) && starts_with($location, '^:')) {
+        if (is_string($location) && Str::startsWith($location, '^:')) {
             $location = '#';
         }
 
