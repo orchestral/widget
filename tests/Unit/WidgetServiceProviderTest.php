2b11dbe8472483a4317062b141b787653396ccb4
Mior Muhammad Zaki <crynobone@gmail.com>
2019-01-29 14:02:47 +0800

    Add additional tests.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/Unit/WidgetServiceProviderTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Orchestra\Widget\TestCase\Unit;
+
+use PHPUnit\Framework\TestCase;
+use Orchestra\Widget\WidgetServiceProvider;
+
+class WidgetServiceProviderTest extends TestCase
+{
+    /** @test */
+    public function it_deferred_the_service_registration()
+    {
+        $stub = new WidgetServiceProvider(null);
+
+        $this->assertTrue($stub->isDeferred());
+    }
+
+    /** @test */
+    public function it_provides_expected_services()
+    {
+        $stub = new WidgetServiceProvider(null);
+
+        $this->assertContains('orchestra.widget', $stub->provides());
+    }
+}
